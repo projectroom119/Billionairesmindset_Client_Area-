d018f1579d5a48cc401820683cd4cb6b9a8f9c16
Ubong <108718734+projectroom119@users.noreply.github.com>
2024-01-26 15:32:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const spinBtn = document.getElementById("spin-btn");
 const finalValue = document.getElementById("final-value");
 //Object that stores values of minimum and maximum angle for a value
 const rotationValues = [
-  { minDegree: 0, maxDegree: 30, value: 2 },
+   { minDegree: 0, maxDegree: 30, value: 2 },
   { minDegree: 31, maxDegree: 90, value: 1 },
   { minDegree: 91, maxDegree: 150, value: 6 },
   { minDegree: 151, maxDegree: 210, value: 5 },
